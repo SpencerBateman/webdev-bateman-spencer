2dd1b798826524d870216923b62773fb468ff593
Spencer Bateman <bateman.s@husky.neu.edu>
2017-10-10 18:44:14 -0400

    cleaning up homepage, started working on routing and building the components

--- src/app/components/user/login/login.component.ts
+++ src/app/components/user/login/login.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  title: string; //see usage as string interpolation
+  disabledFlag: boolean;
+  inputText: string;
+
   constructor() { }
 
   ngOnInit() {
+    this.title = 'This is the login page';
+    this.disabledFlag = true;
   }
 
+  // binding click event
+  buttonClicked(event: any) {
+    console.log(event); // your custom code on button click
+  }
 }
