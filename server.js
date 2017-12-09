953dab9f11b2c1aff823bd281f3e2c1e05f0ffcb
Spencer Bateman <bateman.s@husky.neu.edu>
2017-12-09 17:38:52 -0500

    test

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'dist')));
 
 // CORS
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "http://webdev-bateman-spencer.herokuapp.com");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Credentials", "true");
